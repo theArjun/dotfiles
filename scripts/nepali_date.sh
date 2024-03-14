6781c694dc827ce5359ed7ccd5bd0fb0bc970cfe
thearjun <33110520+theArjun@users.noreply.github.com>
2024-03-14 13:03:09 +0545

    feat: add calendar icon

--- scripts/nepali_date.sh
+++ scripts/nepali_date.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Print the output of the nepcal command for tmux-powerline
+
+calendar_icon=""
+
 run_segment() {
 	nepcal_output=$(nepcal d | awk '{for(i=1;i<NF;i++) printf "%s ", $i; printf "\n"}')
-	echo "${nepcal_output}"
+	echo "${calendar_icon}  ${nepcal_output}"
 }
