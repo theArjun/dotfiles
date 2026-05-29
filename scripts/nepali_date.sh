ea306357680f2fc1ce8efbc0b52297fd0a34955b
thearjun <33110520+theArjun@users.noreply.github.com>
2026-05-29 12:27:30 +0545

    chore: remove obselete scripts

--- scripts/nepali_date.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Print the output of the nepcal command for tmux-powerline
-
-calendar_icon=""
-
-run_segment() {
-	nepcal_output=$(nepcal d | awk '{for(i=1;i<NF;i++) printf "%s ", $i; printf "\n"}')
-	echo "${calendar_icon}  ${nepcal_output}"
-}
--- scripts/nepse.sh
+++
@@ -1,9 +0,0 @@
-stock_icon="󰠟"
-
-run_segment() {
-	working_dir="$HOME/workspace/misc/nepse-api/"
-	cd $working_dir
-	source .venv/bin/activate
-	echo -n $stock_icon
-	PYTHONWARNINGS=ignore python3 test.py
-}
